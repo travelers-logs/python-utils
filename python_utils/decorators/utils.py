7bdf216f5ccd0ab3b5aea52d75077ad2f18c9d55
Ryan Bui <ryan.bui@gmail.com>
2024-10-30 13:57:42 -0700

    add timeit decorator

---
+++ python_utils/decorators/utils.py
@@ -0,0 +1,20 @@
+import time
+
+from logging.logging import init_logger
+
+logger = init_logger()
+
+def timeit(fn):
+    '''
+    Description: decorator function that times the functions process time
+
+    Args:
+        - fn: the function being passed
+    '''
+    # *args and **kwargs are to support positional and named arguments of fn
+    def get_time(*args, **kwargs): 
+        start = time.time() 
+        output = fn(*args, **kwargs)
+        logger.info(f"Time taken in {fn.__name__}: {time.time() - start:.7f}")
+        return output  # make sure that the decorator returns the output of fn
+    return get_time 
